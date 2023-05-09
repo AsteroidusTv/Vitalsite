03faee0c1a5805d3b0e0807abb69bbc944e17e78
AstroidusTv <asteroidus@protonmail.com>
2023-05-09 15:22:02 +0200

    message

---
+++ index.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll',() => {
+
+    if (window.scrollY > 120) {
+        nav.style.top = 0;
+    } else {
+        nav.style.top = "-80px";
+    }
+});
